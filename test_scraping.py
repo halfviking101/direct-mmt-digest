d2b0322237f53ee1b7429a467e978546a1ca6c73
Advik Singhania <advik.singhania@hotelsuperhero.com>
2024-02-01 10:53:18 +0530

    Storing the data, number of success and failures in concurrent scraping script

--- test_scraping.py
+++ test_scraping.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-import json
-from bs4 import BeautifulSoup
 import sys
+import json
+import requests
+import time
+import concurrent.futures
+from bs4 import BeautifulSoup
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-import requests
-import concurrent.futures
-import os
 
 header = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
@@ -25,21 +25,40 @@ except:
     sys.exit(0)
 
 
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i : i + n]
+
+
 def send_requests(mmt_id, trb_id):
     mmt_url = f"https://www.makemytrip.com/hotels/hotel-details/?hotelId={mmt_id}&checkin={check_in_date}&checkout={check_out_date}"
     trb_url = f"https://www.treebo.com/api/v5/hotels/{trb_id}/details/"
     response = s.get(mmt_url, cookies=cookie, headers=header)
     soup = BeautifulSoup(response.content, "html.parser")
     if soup.title == "Site Maintenance":
-        return {"status": 404}
+        return {"status": 404, "url": mmt_url}
 
     sc_tags = soup.find_all("script")
-    data = json.loads(sc_tags[1].string[27:])
+    data = None
+    for val in sc_tags:
+        if val.string and val.string[:27] == "window.__INITIAL_STATE__ = ":
+            data = json.loads(val.string[27:])
+            break
+
+    # data = json.loads(sc_tags[1].string[27:])
+    if (
+        data is None
+        or data["hotelDetail"].get("staticDetail") is None
+        or data["hotelDetail"]["staticDetail"].get("hotelDetails") is None
+    ):
+        return {"status": 404, "url": mmt_url}
+
     mmtData = data["hotelDetail"]["staticDetail"]["hotelDetails"]
     try:
         trbData = requests.get(trb_url).json()["data"]
     except:
-        return {"status": 404}
+        return {"status": 404, "url": trb_url}
 
     return {"mmt": mmtData, "trb": trbData}
 
@@ -47,24 +66,33 @@ def send_requests(mmt_id, trb_id):
 with open("mmt_trb.json") as f:
     mmt_trb = json.load(f)
 
-# mmt_trb = {"202309291210185381": "3890", "202309291227363579": "3889"}
-if not os.path.exists("fetched_data"):
-    os.mkdir("fetched_data")
-
+success, fails = 0, 0
+start_time = time.time()
+merged_data = tuple()
+failed = []
 with concurrent.futures.ThreadPoolExecutor(max_workers=40) as tpe:
-    ftu = {
-        tpe.submit(send_requests, mmt_id, trb_id): (mmt_id, trb_id)
-        for mmt_id, trb_id in mmt_trb.items()
-    }
+    for batch in chunks(list(mmt_trb.items())[:5], 5):
+        ftu = {
+            tpe.submit(send_requests, mmt_id, trb_id): (mmt_id, trb_id)
+            for mmt_id, trb_id in batch
+        }
 
-    for f in concurrent.futures.as_completed(ftu):
-        mmt_id, trb_id = ftu[f]
-        data = f.result()
-        if data.get("status") == 404:
-            print("Failed for MMT", mmt_id, "and TRB", trb_id)
-            continue
-        else:
-            print("Success for", trb_id)
+        for f in concurrent.futures.as_completed(ftu):
+            mmt_id, trb_id = ftu[f]
+            data = f.result()
+            if data.get("status") == 404:
+                # print("Failed for MMT", mmt_id, "and TRB", trb_id)
+                failed.append((mmt_id, trb_id))
+                fails += 1
+                continue
+            else:
+                success += 1
 
-        with open(f"fetched_data/{trb_id}.json", "w") as f:
-            json.dump(data, f, indent=2)
+            merged_data += (data,)
+
+print("fails:", fails, "success:", success)
+print("size:", sys.getsizeof(merged_data) / 1024, "KB")
+print("--- %s seconds ---" % (time.time() - start_time))
+
+if len(failed) != 0:
+    print("Failed:", *failed, sep="\n")
